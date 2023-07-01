fd6ca366e7db322862b409e670df48003df6fd00
Kerem Esen <kerem.esen.01@hotmail.com>
2023-07-02 01:17:17 +0300

    Theme Added

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import React from "react";
 import styles from "./navbar.module.css";
+import DarkModeToggle from "../DarkModeToggle/DarkModeToggle";
 const links = [
   {
     id: 1,
@@ -41,6 +42,7 @@ const Navbar = () => {
         blogz
       </Link>
       <div className={styles.links}>
+        <DarkModeToggle />
         {links.map((link) => (
           <Link key={link.id} href={link.url} className={styles.link}>
             {link.title}
