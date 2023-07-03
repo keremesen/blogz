43c74983acb53586b27b1dcd50ca7146a4b6b15c
Kerem Esen <kerem.esen.01@hotmail.com>
2023-07-03 23:08:47 +0300

    Added Mongoose and Create Blog Page

---
+++ src/models/Post.js
@@ -0,0 +1,38 @@
+import mongoose from "mongoose";
+
+const { Schema } = mongoose;
+
+const postSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    desc: {
+      type: String,
+      required: true,
+    },
+    img: {
+      type: String,
+      required: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+let PostModel;
+try {
+  PostModel = mongoose.model("Posts");
+} catch (error) {
+  PostModel = mongoose.model("Posts", postSchema);
+}
+
+export default PostModel;
